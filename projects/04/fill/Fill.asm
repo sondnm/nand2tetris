f581952cb45f5fce7c939716de02677f5e636914
Son Dang <sondangdev@gmail.com>
2018-07-08 00:13:21 +0700

    [Ex04] Implementation

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,14 +1,43 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+	@8192 // total pixels (512 * 32) + (256 / 16)
+	D=A
+	@count // set count = 8192
+	M=D
+(LOOP)
+	@index // set index = 0 as initial value
+	M=0
+(CHECK)
+	@KBD // compare value in keyboard address, if equal to 0 then jump to white
+	D=M
+	@WHITE
+	D;JEQ
+(BLACK)
+	@index
+	D=M
+	@SCREEN // access value in address {index}th from screen
+	A=A+D
+	M=-1 // set value to -1 which equal 1111111111111111
+	@END
+	0;JMP
+(WHITE)
+	@index
+	D=M
+	@SCREEN
+	A=A+D
+	M=0 // set value to 0
+(END)
+	@index // increment index
+	MD=M+1
+	@count
+	D=D-M
+	@LOOP // if index == count, jumpt to loop
+	D;JEQ
+	@CHECK // else check again
+	0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -1,9 +1,23 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
-
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+	@R2
+	M=0    // Set 0 as intial RAM[2] value (aka sum value)
+(LOOP)
+	@R1
+	D=M    // Load current RAM[1] aka counter value
+	@END
+	D;JEQ  // Jump to END if count == 0
+	@R2
+	D=M    // Load current sum value
+	@R0
+	D=D+M  // Accumulate current sum value with RAM[0] value
+	@R2
+	M=D    // Store result in RAM[2]
+	@R1
+	M=M-1  // Decrement counter by 1
+	@LOOP
+	0;JMP  // Jump back to loop
+(END)
+	@END
+	0;JMP  // Loop forever (a way to terminate program)
